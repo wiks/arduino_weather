ebef3ea4b6575c6c1a6a8aaf9ba0f781522956d8
WikS.eu <github@wiks.eu>
2018-07-16 18:13:42 +0200

    Create run.py

---
+++ run.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+
+import serial
+
+wifi_ssid = "WikS_mobileH"
+wifi_passwd = "***"  # TODO put here your WiFi password
+
+usb_as_serial = '/dev/ttyACM0'
+
+ser = serial.Serial(usb_as_serial, 115200, timeout=2)
+ser.flushInput()
+ser.flushOutput()
+
+while True:
+    data_raw = ser.readline()
+    if len(data_raw) > 0:
+        print(data_raw)
+        if "wifi credientals" in data_raw:
+            toprint = "wifi:" + wifi_ssid + ":" + wifi_passwd + ":wifi"
+            print toprint
+            ser.write(toprint)
